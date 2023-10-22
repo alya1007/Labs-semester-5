908de179349dbb812d5730b87bae791599fa72e3
alya1007 <alexandra.konjevic@isa.utm.md>
2023-10-22 17:49:36 +0300

    lab2

--- PR/lab2/factory.py
+++ PR/lab2/factory.py
@@ -1,26 +1,25 @@
 from player import Player
+from typing import List
 import xml.etree.ElementTree as ET
 import player_pb2 as PlayersList
 
 
 class PlayerFactory:
-    def to_json(self, players):
-        # convert list of Player objects to list of dictionaries
-
-        for player in players:
-            player_list = []
-        for player in players:
-            player_dict = vars(player)
-            player_dict["date_of_birth"] = player_dict["date_of_birth"].strftime(
-                "%Y-%m-%d"
+    def to_json(self, players: List[Player]):
+        return list(
+            map(
+                lambda player: {
+                    "nickname": player.nickname,
+                    "email": player.email,
+                    "date_of_birth": player.date_of_birth.strftime("%Y-%m-%d"),
+                    "xp": player.xp,
+                    "class": player.cls,
+                },
+                players,
             )
-            player_list.append(player_dict)
-        return player_list
+        )
 
     def from_json(self, list_of_dict):
-        """
-        This function should transform a list of dictionaries into a list with Player objects.
-        """
         players = []
         for player_dict in list_of_dict:
             player = Player(
@@ -56,9 +55,6 @@ class PlayerFactory:
         return arr
 
     def to_xml(self, list_of_players):
-        """
-        This function should transform a list with Player objects into a XML string.
-        """
         root = ET.Element("data")  # Create the root element once
         for player in list_of_players:
             player_root = ET.SubElement(root, "player")
@@ -75,9 +71,6 @@ class PlayerFactory:
         return xml_string
 
     def from_protobuf(self, binary):
-        """
-        This function should transform a binary protobuf string into a list with Player objects.
-        """
         player_list = PlayersList.PlayersList()
         player_list.ParseFromString(binary)
         players_obj_list = []
@@ -95,9 +88,6 @@ class PlayerFactory:
         return players_obj_list
 
     def to_protobuf(self, list_of_players):
-        """
-        This function should transform a list with Player objects intoa binary protobuf string.
-        """
         players_list = PlayersList.PlayersList()
         for player in list_of_players:
             players_list.player.add(
